348541d2ce469b94661e837f9d210621d5f70db9
Neocode-1 <neocode.g1@gmail.com>
2025-11-12 22:09:31 +0700

    Belajar membuat hello world, variabel, dan Biodata Dasar

---
+++ Basic Pyhton/Variabel.py
@@ -0,0 +1,10 @@
+#. Membuat progrm yang menampilakn data Diri.
+nama = "Budi Arie"
+umur = 34
+tinggi = 169.6
+is_pejabat = False
+
+print(f"Nama: {nama} ")
+print(f"Umur: {umur} tahun")
+print(f"Tinggi: {tinggi} cm")
+print(f"Pejabar: {is_pejabat}")
